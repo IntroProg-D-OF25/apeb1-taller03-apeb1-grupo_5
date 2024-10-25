a92f972c2ccf225f5329cf0e799fc6993743022c
erickms5 <erick.emelec.m@gmail.com>
2024-10-25 01:52:27 -0500

    Ejercicios lider + 09

---
+++ Proyecto_Taller3/src/Ejercicio09.java
@@ -0,0 +1,16 @@
+public class Ejercicio09 {
+    public static void main(String[] args) {
+        double v1, v2, v3, v4;
+        boolean resultado;
+        
+        v1 = Math.sqrt(81);
+        v2 = Math.sqrt(25);
+        v3 = 10;
+        v4 = 50;
+        
+        resultado = ((v1+v2)*v3) >= v4;
+        System.out.println("resultado = " + resultado);
+              
+    }
+      
+}
